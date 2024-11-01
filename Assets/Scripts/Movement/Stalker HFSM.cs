4345e0610ea42ff78d3fa2e08921a79976c7c89e
Burak <burak.foto@bahcesehir.edu.tr>
2024-11-01 23:53:35 +0300

    oyun
    ilk push

---
+++ Assets/Scripts/Movement/Stalker HFSM.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityHFSM;
+
+public class StalkerHFSM : MonoBehaviour
+{
+    #region Private
+
+    
+
+    #endregion
+
+    #region Public
+
+    
+
+    #endregion
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
